0c3310141d8b5b7a1a6c1e438e10cea2bf99764f
Bruno Vieira <vieiraonwork@gmail.com>
2020-03-19 22:05:02 +0000

    Acrescentada uma mensagem de Boa Tarde

--- Primeiro/Program.cs
+++ Primeiro/Program.cs
@@ -7,6 +7,7 @@ namespace Primeiro
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa Tarde!");
         }
     }
 }
